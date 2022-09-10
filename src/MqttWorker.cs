289dd261b7e86c747a2af2064a99aab17dea5758
leo <leo@home>
2022-09-10 14:47:53 +0100

    Added write delay to cache readings and not to write every time a message is received.

--- src/MqttWorker.cs
+++ src/MqttWorker.cs
@@ -9,11 +9,29 @@ using LiteDB;
 
 namespace MqttLoggerLite
 {
-    public class MqttWorker : BackgroundService
+    public class MqttWorker : BackgroundService, IDisposable
     {
+        // don't want to write every single time, but rather cache for a time
+        const int DELAYMINUTES = 120;
+
         private readonly ILogger<MqttWorker> _logger;
         IMqttClient? _mqttClient;
         WorkerSettings _workerSettings;
+
+        private static DateTime? _lastWrite;
+        private DateTime LastWrite
+        {
+            // if no writes yet, trigger a write
+            get => _lastWrite ??= DateTime.Now.AddMinutes(-(DELAYMINUTES + 1));
+            set { _lastWrite = value; }
+        }
+
+        private static List<TasmotaSensor>? _readings;
+        private List<TasmotaSensor> Readings
+        {
+            get => _readings ??= new List<TasmotaSensor>();
+            set { _readings = value; }
+        }
 
         public MqttWorker(ILogger<MqttWorker> logger)
         {
@@ -63,20 +81,60 @@ namespace MqttLoggerLite
 
             var payload = Encoding.UTF8.GetString(e.ApplicationMessage.Payload);
             var obj = System.Text.Json.JsonSerializer.Deserialize<TasmotaSensor>(payload);
-            WriteObject(obj);
+            SaveObject(obj);
             _logger.LogInformation(payload);
 
             return Task.CompletedTask;
         }
 
-        private void WriteObject(TasmotaSensor data)
+        private void SaveObject(TasmotaSensor data)
         {
+            // add the latest reading first
+            Readings.Add(data);
+
+            if (DateTime.Now.Subtract(LastWrite).TotalMinutes > DELAYMINUTES)
+            {
+                _logger.LogInformation("Time to write");
+                // also save the latest reading
+                LastWrite = DateTime.Now;
+
+                WriteObjects();
+
+                // clean-up cache
+                Readings.Clear();
+            }
+            else
+            {
+                _logger.LogInformation($"No write yet. LastWrite={LastWrite}");
+            }
+        }
+
+        private void WriteObjects()
+        {
+            if (Readings.Count < 1) return;
+
             using (var db = new LiteDatabase(_workerSettings.LiteDbFile))
             {
                 var col = db.GetCollection<TasmotaSensor>("tasmota");
-                col.Insert(data);
+                foreach (var item in Readings)
+                {
+                    col.Insert(item);
+                }
                 col.EnsureIndex(x => x.Time);
             }
         }
+
+        #region IDisposable
+        public override void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            WriteObjects();
+        }
+        #endregion
     }
 }
